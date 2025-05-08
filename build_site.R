7a6b78b34005edd1c64f582fe38e16f40630427d
Maria <maria.cardelino@emory.edu>
2025-05-08 11:01:14 -0400

    Finalize local update

--- build_site.R
+++ build_site.R
@@ -7,22 +7,10 @@ if(length(new_packages)) install.packages(new_packages)
 
 # Load the rmarkdown package
 library(rmarkdown)
+library(knitr)
 
-# Create an images directory if it doesn't exist
-if(!dir.exists("images")) {
-  dir.create("images")
-  cat("Created 'images' directory. Please add your image files to this directory.\n")
-}
 
 # Build the site
 cat("Building the website...\n")
 render_site()
 
-cat("\nWebsite built successfully! The files are in the 'docs' directory.\n")
-cat("To deploy to GitHub Pages:\n")
-cat("1. Push this repository to GitHub\n")
-cat("2. Go to Settings > Pages in your GitHub repository\n")
-cat("3. Set Source to 'Deploy from a branch'\n")
-cat("4. Select 'main' branch and '/docs' folder\n")
-cat("5. Click 'Save'\n\n")
-cat("Your website will be available at https://[your-username].github.io/[repository-name]/\n")
